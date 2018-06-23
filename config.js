2c7f9d4afae3b20a8f3cd1f6daec5a5be4f58cd3
sperixnius <40529037+sperixnius@users.noreply.github.com>
2018-06-23 18:44:42 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    token: 'NDYwMTU0NDUxNDM3NjgyNjkw.DhAnvg.FpkAJCwAzccVZBmpOqUXdWd9qkQ',
+    token: 'NDYwMjExNDkxODg3NTEzNjAx.DhBc3A.Cp4cciutO1kojbscT6hk8hGmFi0',
     GOOGLE_SEARCH: 'AIzaSyDy17ELb7FOuBK-4FouTnvnMnC6j1ga5Ek'
 };
